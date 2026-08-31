8692df716c9f186d28327045a573772f9a067789
JhosuaS <jhosua_saa2412@hotmail.com>
2026-08-31 10:28:51 -0500

    Interfaz fecha-proceso
    firmas de métodos para el modulo

---
+++ src/domain/interfaces/fecha-proceso.interface.ts
@@ -0,0 +1,10 @@
+import { FechaProceso, TipoProceso } from '../entities/fecha-proceso.entity';
+
+export interface IFechaProcesoRepository {
+  create(fechaProceso: Partial<FechaProceso>): Promise<FechaProceso>;
+  findById(id: number): Promise<FechaProceso | null>;
+  findAll(): Promise<FechaProceso[]>;
+  update(id: number, fechaProceso: Partial<FechaProceso>): Promise<FechaProceso>;
+  delete(id: number): Promise<void>;
+  findLatestByProceso(proceso: TipoProceso): Promise<FechaProceso | null>;
+}
