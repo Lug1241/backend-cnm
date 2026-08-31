8b24bb5cf5c127e925d6d98ef229889865fd80b9
JhosuaS <jhosua_saa2412@hotmail.com>
2026-08-31 11:31:30 -0500

    Repository fecha-proceso
    Declaración de métodos, findAll puede encontrar todos los procesos por tipo si se envía como parámetro. Modificación de firma de función en la interfaz

--- src/domain/interfaces/fecha-proceso.interface.ts
+++ src/domain/interfaces/fecha-proceso.interface.ts
@@ -3,7 +3,7 @@ import { FechaProceso, TipoProceso } from '../entities/fecha-proceso.entity';
 export interface IFechaProcesoRepository {
   create(fechaProceso: Partial<FechaProceso>): Promise<FechaProceso>;
   findById(id: number): Promise<FechaProceso | null>;
-  findAll(): Promise<FechaProceso[]>;
+  findAll(page: number, limit: number, proceso: TipoProceso): Promise<{ data: FechaProceso[]; totalRows: number }>;
   update(id: number, fechaProceso: Partial<FechaProceso>): Promise<FechaProceso>;
   delete(id: number): Promise<void>;
   findLatestByProceso(proceso: TipoProceso): Promise<FechaProceso | null>;
