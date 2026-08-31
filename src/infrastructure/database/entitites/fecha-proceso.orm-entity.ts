dcf232461737e202f0cb9fcb0671f429ff0631f2
JhosuaS <jhosua_saa2412@hotmail.com>
2026-08-31 10:37:15 -0500

    orm fecha-proceso
    definición de columnas y tipos de la entidad

---
+++ src/infrastructure/database/entitites/fecha-proceso.orm-entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { TipoProceso } from '../../../domain/entities/fecha-proceso.entity';
+
+@Entity('fechas_procesos')
+export class FechaProcesoOrmEntity {
+  @PrimaryGeneratedColumn( { name: 'ID'} )
+  id!: number;
+
+  @Column({ type: 'date', name: 'fecha_proceso' })
+  fechaProceso!: Date;
+
+  @Column({ type: 'enum', enum: TipoProceso })
+  proceso!: TipoProceso;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  descripcion!: string;
+
+  @CreateDateColumn({ type: 'datetime', name: 'createdAt' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'datetime', name: 'updatedAt' })
+  updatedAt!: Date;
+}
