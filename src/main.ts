9215ca40a6379b33f5212f05e286e79311ff33db
Bryan <bryanlug2014@gmail.com>
2026-09-15 11:37:55 -0500

    cambio de puerto

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: 'http://localhost:5000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
